9b8329b23055673d81915a20ba22e3060a5d78e0
Anbu <a.arunanbu25@gmail.com>
2022-01-10 17:01:44 +0530

    ADDED: New Samples

---
+++ C#/CovidSpread/CovidSpread/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CovidSpread
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int nNumber;
+			Console.WriteLine("Enter the Number");
+			nNumber = int.Parse(Console.ReadLine());	//determine the number of inputs
+
+			while(Convert.ToBoolean(nNumber--))
+			{
+				double a, b, s = 1;
+				a = Convert.ToDouble(Console.ReadLine());
+				b = Convert.ToDouble(Console.ReadLine());
+
+				for(int i = 1; i <= b && s < a; i++)
+				{
+					if(i < 11)
+						s *= 2;
+					else
+						s = a;
+				}
+
+				if(s >= a)
+					s = a;
+				Console.WriteLine(s);
+				Console.WriteLine(" ");
+			}
+		}
+	}
+}
---
+++ C#/KeplersLaw/KeplersLaw/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace KeplersLaw
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int nNumbers;
+			Console.WriteLine("Enter the values of num");
+			nNumbers = int.Parse(Console.ReadLine());
+
+			while(Convert.ToBoolean(nNumbers--))
+			{
+				double T1, T2, R1, R2, a, b;
+				T1 = Convert.ToDouble(Console.ReadLine());
+				T2 = Convert.ToDouble(Console.ReadLine());
+				R1 = Convert.ToDouble(Console.ReadLine());
+				R2 = Convert.ToDouble(Console.ReadLine());
+
+				a = Math.Pow(T1, 2) / Math.Pow(R1, 3);
+				b = Math.Pow(T2, 2) / Math.Pow(R2, 3);
+
+				if(a == b)
+					Console.WriteLine("Yes");
+				else
+					Console.WriteLine("No");
+			}
+		}
+	}
+}
