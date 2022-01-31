68b1134c3e67773a24be3a29954ad365b04eb084
Anbu <a.arunanbu25@gmail.com>
2022-01-31 11:11:33 +0530

    ADDED: New Exercise

---
+++ C#/EnormousInputTest/EnormousInputTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EnormousInputTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n, k, nCount = 0;
+			Console.WriteLine("Enter the n");
+			n = int.Parse(Console.ReadLine());
+			Console.WriteLine("Enter the k");
+			k = int.Parse(Console.ReadLine());
+			Console.WriteLine("The Input numbers are ");
+
+			for(int i = 0; i < n ; i++)
+			{				
+				int nInput = int.Parse(Console.ReadLine());
+				if(nInput % k == 0)
+				{
+					nCount++;
+				}
+			}
+
+			Console.WriteLine("The Total count value is " +nCount);
+		}
+	}
+}
---
+++ C#/Prime in Binarystring/Prime in Binarystring/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Prime_in_Binarystring
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int nNumber;
+			Console.WriteLine("Enter the number");
+			nNumber = int.Parse(Console.ReadLine());
+
+			while(Convert.ToBoolean(nNumber--))
+			{
+				int nlength;
+				string s;
+				s = Console.ReadLine();
+				nlength = s.Length;
+
+				if(nlength == 1)
+					Console.WriteLine("No");
+				//continue;
+
+				int nCount = 0, nPosition = -1, i = 0;
+				while(i < nlength)
+				{
+					if(s[i] == '1')
+					{
+						nCount++;
+						nPosition = i + 1;
+					}
+					else i++;
+				}
+
+				if( nCount == 0 || (nCount == 1 && nPosition == nlength))
+				{
+					Console.WriteLine("No");
+				}
+				else Console.WriteLine("Yes");
+			}
+		}
+	}
+}
