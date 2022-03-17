5b85238830f8b0c7eacd6c84c2fb0e93db431269
Anbu <a.arunanbu25@gmail.com>
2022-03-17 18:04:18 +0530

    ADDED: New Exercise

---
+++ C#/MajorityElement/MajorityElement/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MajorityElement
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int nLength, i;
+			Console.WriteLine("Enter Length");
+			nLength = int.Parse(Console.ReadLine());
+			int[] nArray = new int[nLength];
+
+			Console.WriteLine("Enter Array Values ");
+
+			for(i = 0; i < nLength; i++)
+			{
+				nArray[i] = int.Parse(Console.ReadLine());
+			}
+
+			Console.WriteLine("Output is " + MajorityElement(nArray));
+		}
+	
+		static int MajorityElement(int[] nArray)
+		{
+			int i, j, nCount, nLength;
+			nLength = nArray.Length;
+			for(i = 0; i < nLength; i++)
+			{
+				nCount = 1;
+				for(j = 1; j < nLength; j++)
+				{
+					if(nArray[i] == nArray[j])
+					{
+						nCount++;
+					}
+				}
+
+				if(nCount > nLength / 2)
+				{
+					return nArray[i];
+				}			
+			}
+			return -1;
+		}
+	}
+}
+
+
---
+++ C#/MinAbsSum/MinAbsSum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MinAbsSum
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int nLength, i;
+			Console.WriteLine("Enter Array Length");
+			nLength = int.Parse(Console.ReadLine());
+			int[] nArray = new int[nLength];
+			Console.WriteLine("Enter Array Values");
+
+			for(i = 0; i < nLength; i++)
+			{
+				nArray[i] = int.Parse(Console.ReadLine());
+			}
+
+			Array.Sort(nArray);
+			
+			int nMinDiff = nArray[i - 1] - nArray[0];
+			Console.WriteLine("The mD value is " +nMinDiff);
+
+			for(i = 0; i < nLength - 1; i++)
+			{
+				int nTempDiff = nArray[i + 1] - nArray[i];
+				if(nTempDiff < nMinDiff)
+				{
+					nMinDiff = nTempDiff;
+				}
+			}
+			Console.WriteLine("The MinDiff is " +nMinDiff);
+		}
+	}
+}
